484d409d1153f09df17614aaf3337369ccc67cfa
Avadheshkumarchauhan <avadheaha@gmail.com>
2026-04-13 11:27:08 +0530

    vercel file config

--- Soket/soket.js
+++ Soket/soket.js
@@ -9,39 +9,46 @@ import express from "express";
 import {Server} from "socket.io"
 const app = express();
 
-const server = http.createServer(app);
-
-const io = new Server(server,{
-    cors:{
-        origin:[process.env.FRONTEND_URL],
-        credentials:true
-    }
-});
-
-const userSocketMap ={};
-export const getReceiverSocketId =(receiver)=>{
-    return userSocketMap[receiver];
-}
-io.on("connection",(socket)=>{
-
-    const userId = socket.handshake.query.userId;
-     //console.log("UserId : ",userId);
-
-    if(userId !=="undefined"){
-        userSocketMap[userId]=socket.id;
-
-        console.log("UserId : ",userId);
-         console.log("socketId : ",socket?.id);
-    }
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
-    console.log(userSocketMap);   
-    // io.emit("hello","Avadhesh")
-    socket.on("disconnect",()=>{
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    
+    const server = http.createServer(app);
+    
+    const io = new Server(server,{
+        cors:{
+            origin:[process.env.FRONTEND_URL],
+            credentials:true
+        }
     });
     
-});
-
+    const userSocketMap ={};
+    export const getReceiverSocketId =(receiver)=>{
+        return userSocketMap[receiver];
+    }
+    try {
+    io.on("connection",(socket)=>{
+    
+        const userId = socket.handshake.query.userId;
+         //console.log("UserId : ",userId);
+    
+        if(userId !=="undefined"){
+            userSocketMap[userId]=socket.id;
+    
+            console.log("UserId : ",userId);
+             console.log("socketId : ",socket?.id);
+        }
+        io.emit("getOnlineUsers",Object.keys(userSocketMap));
+        console.log(userSocketMap);   
+        // io.emit("hello","Avadhesh")
+        socket.on("disconnect",()=>{
+            delete userSocketMap[userId];
+            io.emit("getOnlineUsers",Object.keys(userSocketMap));
+        });
+        
+    });
+    
+} catch (error) {
+    console.log(error.message);
+    
+    
+}
 
 export {app ,server,io};
